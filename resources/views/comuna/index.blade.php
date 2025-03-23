780b71f2ec7db64f0ab3f24bd1ab4c71b92645ce
juanposada25 <juandiegoposada245@gmail.com>
2025-03-23 17:51:51 -0500

    Commune Edit

--- resources/views/comuna/index.blade.php
+++ resources/views/comuna/index.blade.php
@@ -31,7 +31,10 @@
               <th scope="row">{{ $comuna->comu_codi }}</th>
               <td>{{$comuna->comu_nomb}}</td>
               <td>{{$comuna->muni_nomb}}</td>
-                <td><form action="{{ route('comunas.destroy', ['comuna' => $comuna->comu_codi]) }}" 
+                <td>
+                <a href="{{ route('comunas.edit', ['comuna' => $comuna->comu_codi]) }}"
+                  class="btn btn-info">Edit</a>
+                  <form action="{{ route('comunas.destroy', ['comuna' => $comuna->comu_codi]) }}" 
                       method="POST" style="display: inline-block">
                       @method('delete')
                       @csrf
