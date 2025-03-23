5c6b8e4d04edb7f7986fdbfa9807e1b64789e4fe
juanposada25 <juandiegoposada245@gmail.com>
2025-03-23 16:34:40 -0500

    Commune Add

--- resources/views/comuna/index.blade.php
+++ resources/views/comuna/index.blade.php
@@ -14,6 +14,8 @@
   <body>
     <div class="container">
         <h1>Listado de Comunas</h1>
+
+        <a href="{{ route('comunas.create') }}" class="btn btn-success">Add</a>
         <table class="table">
   <thead>
     <tr>
