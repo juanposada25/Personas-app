785e74ecfa1db3ab4a38393b075d94769c262bf8
juanposada25 <juandiegoposada245@gmail.com>
2025-03-24 00:24:40 -0500

    Municipio Edit

--- app/Http/Controllers/MunicipioController.php
+++ app/Http/Controllers/MunicipioController.php
@@ -63,17 +63,36 @@ class MunicipioController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit($id)
     {
-        //
+        $municipio = Municipio::find($id);
+        $departamentos = DB::table('tb_departamento')
+            ->orderBy('depa_nomb')
+            ->get();
+
+        return view('municipio.edit', [
+            'municipio' => $municipio, 
+            'departamentos' => $departamentos
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, $id)
     {
-        //
+        $municipio = Municipio::find($id);    
+
+        $municipio->muni_nomb = $request->name;
+        $municipio->depa_codi = $request->code;
+        $municipio->save();
+
+        $municipios = DB::table('tb_municipio')
+        ->join('tb_departamento', 'tb_municipio.depa_codi', '=', 'tb_departamento.depa_codi')
+        ->select('tb_municipio.*', 'tb_departamento.depa_nomb')
+        ->get();
+
+    return view('municipio.index', ['municipios' => $municipios]);
     }
 
     /**
