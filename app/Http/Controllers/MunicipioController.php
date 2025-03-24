dbde4cce3c52ce6e387339c1183cc835ebf4c74b
juanposada25 <juandiegoposada245@gmail.com>
2025-03-24 00:01:45 -0500

    Municipios Add

--- app/Http/Controllers/MunicipioController.php
+++ app/Http/Controllers/MunicipioController.php
@@ -79,8 +79,16 @@ class MunicipioController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $municipio = Municipio::find($id);
+        $municipio->delete();
+
+        $municipios = DB::table('tb_municipio')
+        ->join('tb_departamento', 'tb_municipio.depa_codi', '=', 'tb_departamento.depa_codi')
+        ->select('tb_municipio.*', 'tb_departamento.depa_nomb')
+        ->get();
+
+    return view('municipio.index', ['municipios' => $municipios]);
     }
 }
